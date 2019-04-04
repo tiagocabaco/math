383f569b5d0b025309df3788287113fcdd1b6ac3
Edward A. Roualdes <eroualdes@csuchico.edu>
2019-04-04 14:34:17 -0700

    re: change assertion so test passes

--- test/unit/math/prim/fun/digamma_test.cpp
+++ test/unit/math/prim/fun/digamma_test.cpp
@@ -1,9 +1,15 @@
-#include <stan/math/prim/scal.hpp>
+
+
+
+
+
+
+#include <stan/math/prim.hpp>
 #include <boost/math/special_functions/digamma.hpp>
 #include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
-#include <limits>
 
+#include <limits>
 TEST(MathFunctions, digamma) {
   EXPECT_FLOAT_EQ(boost::math::digamma(0.5), stan::math::digamma(0.5));
   EXPECT_FLOAT_EQ(boost::math::digamma(-1.5), stan::math::digamma(-1.5));
@@ -13,6 +19,7 @@ TEST(MathFunctions, digamma_nan) {
   double nan = std::numeric_limits<double>::quiet_NaN();
 
   EXPECT_PRED1(boost::math::isnan<double>, stan::math::digamma(nan));
+EXPECT_THROW(stan::math::digamma(-1), std::domain_error);
 
-  EXPECT_PRED1(boost::math::isnan<double>, stan::math::digamma(-1));
+  
 }
